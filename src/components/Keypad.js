9f21f61866d506e077c72ffb0ae0c33569cb39ed
Imelda Hope <imeldalarusso@gmail.com>
2024-08-02 05:19:28 +0300

    creates event on components

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,13 @@
 // Code Keypad Component Here
 
 function Keypad (){
+    function changeHandler(){
+        console.log('Entering password...')
+    }
     return (
-        <div></div>
+        <div>
+            <input name='password' type="password" placeholder="Enter Password" onChange={changeHandler}></input>
+        </div>
     )
 }
 
